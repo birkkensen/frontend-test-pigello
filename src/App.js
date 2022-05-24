8651ad3730666080b18c5ebc6f6e9869163444da
Birk <birk.kensen@gmail.com>
2022-05-24 21:57:17 +0200

    displaying some data in a table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { fetchFromApi } from './api';
+import { BiSearch } from 'react-icons/bi';
 
 function App() {
 	const [data, setData] = useState();
@@ -8,19 +9,31 @@ function App() {
 	}, []);
 
 	console.log(data);
-	return (
-		data && (
-			<section className='container'>
-				<div>
-					<input className='search-field' placeholder='Search bodies' />
-				</div>
-				<div className='titles'>
-					<h4>Name</h4>
-					<h4>Around Planet</h4>
-					<h4>Body Type</h4>
-				</div>
-			</section>
-		)
+	return data ? (
+		<section className='container'>
+			<div className='search-container'>
+				<BiSearch />
+				<input className='search-field' placeholder='Search bodies' />
+			</div>
+			<div className='titles'>
+				<h4>Name</h4>
+				<h4>Around Planet</h4>
+				<h4>Body Type</h4>
+			</div>
+			<div>
+				{data.bodies.map((bodie) => {
+					return (
+						<div className='bodies'>
+							<p>{bodie.englishName}</p>
+							<p>{bodie.aroundPlanet?.planet}</p>
+							<p>{bodie.bodyType}</p>
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	) : (
+		<div>Loading...</div>
 	);
 }
 
