eb0eef7fd4a634ac8009f04cbe29ed3fe6fbb690
Birk <birk.kensen@gmail.com>
2022-05-24 21:14:15 +0200

    fetch is fetching, apparently cant call the function fetch hehe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import { useState, useEffect } from 'react';
+import { fetchFromApi } from './api';
+
 function App() {
+	const [bodies, setBodies] = useState();
+	useEffect(() => {
+		fetchFromApi().then(setBodies);
+	}, []);
+
+	console.log(bodies);
 	return <div>Hello world:)</div>;
 }
 
