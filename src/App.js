08602bac26787fb9dc4951aaae989cc8a30e2606
Birk <birk.kensen@gmail.com>
2022-05-24 23:05:45 +0200

    super simple search query

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { useState, useEffect } from 'react';
 import { fetchFromApi } from './api';
 import { BiSearch } from 'react-icons/bi';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 	const [data, setData] = useState();
-	useEffect(() => {
-		fetchFromApi().then(setData);
-	}, []);
+	const [query, setQuery] = useState('');
 
-	console.log(data);
-	return data ? (
+	useEffect(() => {
+		fetchFromApi(query).then(setData);
+	}, [query]);
+
+	return (
 		<section className='container'>
 			<div className='search-container'>
 				<BiSearch />
-				<input className='search-field' placeholder='Search bodies' />
+				<input
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+					className='search-field'
+					placeholder='Search bodies'
+				/>
 			</div>
 			<div className='titles'>
 				<h4>Name</h4>
@@ -21,19 +28,20 @@ function App() {
 				<h4>Body Type</h4>
 			</div>
 			<div>
-				{data.bodies.map((bodie) => {
-					return (
-						<div className='bodies'>
-							<p>{bodie.englishName}</p>
-							<p>{bodie.aroundPlanet?.planet}</p>
-							<p>{bodie.bodyType}</p>
-						</div>
-					);
-				})}
+				{data &&
+					data.bodies?.map((bodie) => {
+						return (
+							<div key={uuidv4()} onClick={() => console.log('Click')} className='bodies'>
+								<p className='bodie'>{bodie.englishName}</p>
+								<p className='bodie'>
+									{bodie.aroundPlanet?.planet ? bodie.aroundPlanet?.planet : 'No Planet Data'}
+								</p>
+								<p className='bodie'>{bodie.bodyType}</p>
+							</div>
+						);
+					})}
 			</div>
 		</section>
-	) : (
-		<div>Loading...</div>
 	);
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
-const BASE_URL = 'https://api.le-systeme-solaire.net/rest/bodies/';
-const DATA_ORDER = '?order=aroundPlanet,desc/';
-const fetchFromApi = async () => {
-	const res = await fetch(`${BASE_URL}${DATA_ORDER}`)
+const BASE_URL = 'https://api.le-systeme-solaire.net/rest/bodies';
+const DATA_ORDER = 'order=aroundPlanet,desc';
+const FILTER = 'filter[]=isPlanet,eq,false';
+const fetchFromApi = async (query) => {
+	const res = await fetch(`${BASE_URL}/?${DATA_ORDER}&${FILTER}&filter[]=id,sw,${query}`)
 		.then((data) => data.json())
 		.catch((err) => console.log(err));
 	return res;
